7e6869955d6f94328bd471195283c9134915cc19
ViktorPank <41527815+ViktorPank@users.noreply.github.com>
2020-05-19 02:23:21 +0400

    init

---
+++ src/main/java/com/pankin/the_bull_cow/TheBullCowApplication.java
@@ -0,0 +1,13 @@
+package com.pankin.the_bull_cow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBullCowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBullCowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pankin/the_bull_cow/TheBullCowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pankin.the_bull_cow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBullCowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
